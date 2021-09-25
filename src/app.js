0c45f00622bd281d0348fd619626893e7e4f9815
tinameza <tina.meza94@gmail.com>
2021-09-25 21:47:48 +0200

    Relevant weather icon from API added

--- src/app.js
+++ src/app.js
@@ -54,6 +54,12 @@ function cityWeather(response) {
   document.querySelector(".wind").innerHTML = `Wind: ${Math.round(
     response.data.wind.speed
   )} km/h`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(cityName) {
